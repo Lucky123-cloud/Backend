b1efced1ee8f2530cd26b95883eaaae410823539
Lucky-cloud 123 <luckybaraka@gmail.com>
2025-04-12 11:48:19 +0300

    added python course

---
+++ python/2-print.py
@@ -0,0 +1,9 @@
+# Write a Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+
+# Use the function print
+# guillaume@ubuntu:~/py/0x00$ ./2-print.py 
+# "Programming is like building a multilingual puzzle
+# guillaume@ubuntu:~/py/0x00$
+
+#!/usr/bin/python3
+print("\"Programming is like building a multilingual puzzle")
